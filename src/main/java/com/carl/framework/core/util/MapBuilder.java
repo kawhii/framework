56d48040c02bcdf4fd5c9f8d09f3541228b1090f
Carl <75688443@qq.com>
2016-12-27 13:43:11 +0800

    实现

--- src/main/java/com/carl/framework/core/util/MapBuilder.java
+++ src/main/java/com/carl/framework/core/util/MapBuilder.java
@@ -1,5 +1,7 @@
 package com.carl.framework.core.util;
 
+import java.util.HashMap;
+
 /**
  * @author Carl
  * @date 2016/12/27
@@ -7,5 +9,16 @@ package com.carl.framework.core.util;
  * <p>
  * 版权所有.(c)2008-2016.广州市森锐科技股份有限公司
  */
-public class MapBuilder {
+public class MapBuilder<K, V> extends HashMap<K, V> {
+    private MapBuilder() {
+    }
+
+    public static <K, V> MapBuilder<K, V> build() {
+        return new MapBuilder<K, V>();
+    }
+
+    public MapBuilder<K, V> p(K k, V v) {
+        super.put(k, v);
+        return this;
+    }
 }
